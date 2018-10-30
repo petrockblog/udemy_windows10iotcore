47713f91006f2bafe8167cd63586ededaef76c48
Florian <contact@petrockblock.com>
2018-10-30 21:12:38 +0100

    Using button to switch LED

--- HelloIoTWorld/HelloIoTWorld/StartupTask.cpp
+++ HelloIoTWorld/HelloIoTWorld/StartupTask.cpp
@@ -6,19 +6,43 @@ using namespace HelloIoTWorld;
 using namespace Platform;
 using namespace Windows::ApplicationModel::Background;
 using namespace Windows::Devices::Gpio;
+using namespace Windows::Foundation;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
 void StartupTask::Run(IBackgroundTaskInstance^ taskInstance)
 {
-	auto gpioController = GpioController::GetDefault();   // Gets the default general-purpose I/O (GPIO) controller for the system.
-	GpioPin ^pin = gpioController->OpenPin(18);           // Opens a connection to the specified general-purpose I/O (GPIO) pin in exclusive mode.
-	pin->SetDriveMode(GpioPinDriveMode::Output);          // Sets the drive mode of the general-purpose I/O (GPIO) pin. The drive mode specifies whether the pin is configured as an input or an output, and determines how values are driven onto the pin.
+	bgTaskDeferral = taskInstance->GetDeferral();
 
-	while (true) {
-		pin->Write(GpioPinValue::High);  // Drives the specified value onto the general purpose I/O (GPIO) pin according to the current drive mode for the pin.
-		Sleep(500);                      // Suspends the execution of the current thread until the time-out interval elapses.
-		pin->Write(GpioPinValue::Low);
-		Sleep(500);
+	auto gpioController = GpioController::GetDefault();   // Gets the default general-purpose I/O (GPIO) controller for the system.
+	ledPin = gpioController->OpenPin(LEDPIN);           // Opens a connection to the specified general-purpose I/O (GPIO) pin in exclusive mode.
+	buttonPin = gpioController->OpenPin(BUTTONPIN);
+	
+	const bool allControllersWereCreated = (ledPin != nullptr) && (buttonPin != nullptr);
+	if (allControllersWereCreated) {
+		ledPin->SetDriveMode(GpioPinDriveMode::Output);          // Sets the drive mode of the general-purpose I/O (GPIO) pin. The drive mode specifies whether the pin is configured as an input or an output, and determines how values are driven onto the pin.
+		ledPin->Write(GpioPinValue::High);  // Drives the specified value onto the general purpose I/O (GPIO) pin according to the current drive mode for the pin.
+
+		buttonPin->SetDriveMode(GpioPinDriveMode::InputPullUp);
+		const Windows::Foundation::TimeSpan debounceTime = { BUTTONDEBOUNCETIME };  
+		buttonPin->DebounceTimeout = debounceTime;
+		buttonPin->ValueChanged += ref new TypedEventHandler<GpioPin^, GpioPinValueChangedEventArgs^>(this, &StartupTask::ButtonPin_ValueChanged);
+	}
+	else
+	{
+		bgTaskDeferral->Complete();
+	}
+}
+
+void StartupTask::ButtonPin_ValueChanged(Windows::Devices::Gpio::GpioPin ^sender, Windows::Devices::Gpio::GpioPinValueChangedEventArgs ^args) {
+	if (args->Edge == GpioPinEdge::FallingEdge) {
+		const GpioPinValue currentValue = ledPin->Read();
+		if (currentValue == GpioPinValue::High) {
+			ledPin->Write(GpioPinValue::Low);
+		}
+		else
+		{
+			ledPin->Write(GpioPinValue::High);
+		}
 	}
 }
--- HelloIoTWorld/HelloIoTWorld/StartupTask.h
+++ HelloIoTWorld/HelloIoTWorld/StartupTask.h
@@ -10,5 +10,16 @@ namespace HelloIoTWorld
     public:
         virtual void Run(Windows::ApplicationModel::Background::IBackgroundTaskInstance^ taskInstance);
 
+	private:
+		const int LEDPIN = 18;
+		const int BUTTONPIN = 17;
+		const long BUTTONDEBOUNCETIME = 20 * 10000LL;  // equivalent to 20 ms
+
+		Platform::Agile<Windows::ApplicationModel::Background::BackgroundTaskDeferral> bgTaskDeferral;
+		Windows::Devices::Gpio::GpioPin ^ledPin;
+		Windows::Devices::Gpio::GpioPin ^buttonPin;
+
+		void ButtonPin_ValueChanged(Windows::Devices::Gpio::GpioPin ^sender, Windows::Devices::Gpio::GpioPinValueChangedEventArgs ^args);
+
     };
 }
