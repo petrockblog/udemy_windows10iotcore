2d3f9e6435fe974d654e72a1e3aa2d32a48fc01e
Florian <contact@petrockblock.com>
2018-11-02 18:39:18 +0100

    Changed variable name

--- HelloIoTWorld/HelloIoTWorld/StartupTask.cpp
+++ HelloIoTWorld/HelloIoTWorld/StartupTask.cpp
@@ -18,8 +18,8 @@ void StartupTask::Run(IBackgroundTaskInstance^ taskInstance)
 	ledPin = gpioController->OpenPin(LEDPIN);           // Opens a connection to the specified general-purpose I/O (GPIO) pin in exclusive mode.
 	buttonPin = gpioController->OpenPin(BUTTONPIN);
 	
-	const bool allControllersWereCreated = (ledPin != nullptr) && (buttonPin != nullptr);
-	if (allControllersWereCreated) {
+	const bool allPinsWereOpened = (ledPin != nullptr) && (buttonPin != nullptr);
+	if (allPinsWereOpened) {
 		ledPin->SetDriveMode(GpioPinDriveMode::Output);          // Sets the drive mode of the general-purpose I/O (GPIO) pin. The drive mode specifies whether the pin is configured as an input or an output, and determines how values are driven onto the pin.
 		ledPin->Write(GpioPinValue::High);  // Drives the specified value onto the general purpose I/O (GPIO) pin according to the current drive mode for the pin.
 
